65c54a842bafac677c580ce47a71ea5f372c5645
snehalpattiwar <snehalpattiwar@gmail.com>
2026-04-15 17:34:32 +0530

    Python Fundamentals Practice Set

--- practiceSetPython1.py
+++ practiceSetPython1.py
@@ -1,1 +1,35 @@
+# Python Playground - Edit me!
+
+# Welcome to Python in the browser!
+# Powered by Pyodide (Python compiled to WebAssembly)
+
+print('Hello, World! Welcome to Python.')
+
+print("Twinkle, twinkle, little star,\nHow I wonder what you are!")
+
+name = "Snehal";
+age = 33;
+height = 5.7;
+student = False;
+print("My name is "+str(name)+". I am " +str(age)+" years old.My height is " +str(height)+". Student is:"+str(student))
+
+num = "45"
+print(int(num)+10)
+
+# food = input("Enter your favourite food:\n")
+# print("Wow! I also like" + food)
+
+a = 10 #int(input("Enter first number:"))
+b = 20 #int(input("Enter second number:"))
+print("Addition is:", a + b)
+print("Difference is:", a - b)
+print("Product is:", a*b)
+print("Quotient is:", a/b)
+
+print("Harry said, \"Python is awesome!\"\nThis is on a new line.\nThis is a tab ->\t <- here")
+
+a = 5 #int(input("Enter a number:\n"))
+print("Square is",a**2)
+print('Cube is', a**3)
+
 
