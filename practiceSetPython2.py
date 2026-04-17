2540c3cb358ae46f16ca8af8dd1484bb81a2ca1c
snehalpattiwar <snehalpattiwar@gmail.com>
2026-04-17 10:52:48 +0530

    control flow

--- practiceSetPython2.py
+++ practiceSetPython2.py
@@ -1,1 +1,58 @@
 
+# a = 10 
+# #int(input("Enter any number:"))
+# b = -1
+# c = 0
+# if(a<0):
+#   print("Positive Number")
+# elif b<0:
+#   print("Negative NUmber")
+# else:
+#   print("It is a zero")
+
+# #int(input("Enter a day number (1–7)"))
+# a = 6
+# print(f"Day number is {a}")
+# match a: 
+#   case 1:
+#     print("Day is Monday")
+#   case 2:
+#     print("Day is Tuesday")
+#   case 3:
+#     print("Day is Wednesday")
+#   case 4:
+#     print("Day is Thursday")
+#   case 5:
+#     print("Day is Friday")
+#   case 6:
+#     print("Day is Saturday")
+#   case 7:
+#     print("Day is Sunday")
+
+
+# a = 10
+# b = 20
+# #op = input("Enter a operation you want to perform")
+# # op = "ASMD"
+# op1 = "S"
+# match op1:
+#   case "A":
+#      print(f"Addition is {a+b}")
+#   case "S":
+#      print(f"Substraction is {a-b}")
+#   case "M":
+#      print(f"Multiplication is {a*b}")
+#   case "D":
+#      print(f"Division is {a/b}")
+
+# for i in range(1, 11):
+#    print(i)
+# for i in range(1,11):
+#   print(5*(i))
+sum = 0
+for i in range(1,101):
+  sum = sum + i
+print("Addition of 1 to 100 is",sum)
+for i in range(1,5):
+  print("*"*i)
+
