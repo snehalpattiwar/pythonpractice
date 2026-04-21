5b091bfd4c6352ddda0b2a000a45dae8873ab4a0
snehalpattiwar <snehalpattiwar@gmail.com>
2026-04-21 16:43:49 +0530

    Add string manipulation examples in practiceSet3.py

---
+++ practiceSet3.py
@@ -0,0 +1,13 @@
+
+name = "Snehal IBM Angular"
+print("First Char is: ",name[0])
+print("Last Char is: ",name[len(name) - 1])
+print("Length of name is: ",len(name))
+print("Hello" + " " + "World" )
+
+text = "Python Programming"
+print(text[0:6])
+print(text[-6:])
+print(len(text))
+print(text[::2])
+print(text[::-1])
